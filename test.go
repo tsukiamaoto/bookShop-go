65f5c7b6656d8a1f1d3485617c4abf6ffacb240c
gary <gomrdhl796@gmail.com>
2021-09-24 14:47:02 +0800

    step1: change static page to dynamic page by using html/template

--- test.go
+++ test.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
+type IndexData struct {
+	Title   string
+	Content string
+}
+
+// html page
 func test(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`my first website`))
+	tmpl := template.Must(template.ParseFiles("./index.html"))
+	data := new(IndexData)
+	data.Title = "首頁"
+	data.Content = "我的第一個首頁"
+	tmpl.Execute(w, data)
 }
 
 func main() {
