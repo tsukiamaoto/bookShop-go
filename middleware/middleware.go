50257185adbe6f72090d213b49d9c883d9e08641
gary <gomrdhl796@gmail.com>
2021-11-03 14:29:54 +0800

    add getuserId from redis and refactor

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,14 +1,16 @@
-package auth
+package middleware
 
 import (
 	"net/http"
-	"shopCart/redis"
 	"shopCart/config"
+	"shopCart/redis"
 
-	"github.com/rbcervilla/redisstore/v8"
+	"strconv"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/sessions"
-	"github.com/gin-contrib/cors"
+	"github.com/rbcervilla/redisstore/v8"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -26,20 +28,20 @@ func init() {
 	}
 	store.KeyPrefix("session_")
 	store.Options(sessions.Options{
-			Path:   "/",
-			// Domain: "localhost",
-			MaxAge: 86400 * 7, // 7 days
+		Path: "/",
+		// Domain: "localhost",
+		MaxAge: 86400 * 7, // 7 days
 	})
 
 }
 
 func CorsConfig(conf *config.Config) cors.Config {
-  corsConf := cors.DefaultConfig()
+	corsConf := cors.DefaultConfig()
 	corsConf.AllowOrigins = conf.AllowOrigins
 	// corsConf.AllowCredentials = true
 	// corsConf.AllowAllOrigins = true
 	corsConf.AllowHeaders = []string{"Authorization", "Content-Type", "Upgrade", "Origin",
-			"Connection", "Accept-Encoding", "Accept-Language", "Host", "Access-Control-Request-Method", "Access-Control-Request-Headers"}
+		"Connection", "Accept-Encoding", "Accept-Language", "Host", "Access-Control-Request-Method", "Access-Control-Request-Headers"}
 
 	return corsConf
 }
@@ -61,7 +63,7 @@ func AuthRequired(c *gin.Context) {
 	c.Next()
 }
 
-func SaveToRedis(c *gin.Context) error {
+func SetAuth(c *gin.Context) error {
 	session, err := store.Get(c.Request, "session-key")
 	if err != nil {
 		log.Error("Failed to get session, reason is :", err)
@@ -77,17 +79,53 @@ func SaveToRedis(c *gin.Context) error {
 	return nil
 }
 
-func DeleteFromRedis(c *gin.Context) error {
+func DeleteAuth(c *gin.Context) error {
 	session, err := store.Get(c.Request, "session-key")
 	if err != nil {
 		log.Error("Failed to get session, reason is :", err)
 		return err
 	}
 
-	// redis delete data when Maxage <= 0  
+	// redis delete data when Maxage <= 0
 	session.Options.MaxAge = -1
 	if err = session.Save(c.Request, c.Writer); err != nil {
 		log.Error("Failed to delete session, reason is :", err)
+		return err
+	}
+
+	return nil
+}
+
+func GetUserId(c *gin.Context) (uint, error) {
+	session, err := store.Get(c.Request, "session-key")
+	if err != nil {
+		log.Error("Failed to get session, reason is :", err)
+		return 0, err
+	}
+
+	val := session.Values["userId"]
+
+	userIdOfString, ok := val.(string)
+	if !ok {
+		log.Error("Failed to get session value by key: 'userId'")
+	}
+
+	uid64, _ := strconv.ParseUint(userIdOfString, 10, 64)
+	userId := uint(uid64)
+
+	return userId, err
+}
+
+func SaveToRedis(c *gin.Context, key, value string) error {
+	session, err := store.Get(c.Request, "session-key")
+	if err != nil {
+		log.Error("Failed to get session, reason is :", err)
+		return err
+	}
+
+	session.Values[key] = value
+	if err = session.Save(c.Request, c.Writer); err != nil {
+		log.Error("Failed to save session, reason is :", err)
 		return err
 	}
 
